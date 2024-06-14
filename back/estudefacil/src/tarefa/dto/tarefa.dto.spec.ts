1b836f6fb6e8cf758b7ca30034d51b1efbfd3f8f
Felipe Mori Ferreira <118014066+Felipe-Mori@users.noreply.github.com>
2024-06-14 14:38:31 -0300

    Delete back directory

--- back/estudefacil/src/tarefa/dto/tarefa.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CreateTarefaDto } from './tarefa.dto';
-
-describe('TarefaDto', () => {
-  it('should be defined', () => {
-    expect(new CreateTarefaDto()).toBeDefined();
-  });
-});
--- back/estudefacil/src/user/dto/user.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CreateUserDto } from "./user.dto";
-
-describe('UserDto', () => {
-  it('should be defined', () => {
-    expect(new CreateUserDto()).toBeDefined();
-  });
-});
